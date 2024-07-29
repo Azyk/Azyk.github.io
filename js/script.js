70b60c1be357e9921792dc7889675207db5532b4
Azamat <hifastr@gmail.com>
2024-07-29 16:33:55 +0500

    add direct

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 let audio = document.querySelector('audio');
 let mario = document.querySelector('.mario');
+
+// Время анимации в миллисекундах (например, 300 мс)
+const jumpDuration = 300;
+
 document.addEventListener('click', (event) => {
    if (event.target.closest('.mario-img')) {
       mario.classList.remove('duble_jump');
       mario.classList.add('jump');
       audio.play();
+
+      // Переход на другую страницу после завершения прыжка
+      setTimeout(() => {
+         window.location.href = 'https://azyk.github.io/Game/';
+      }, jumpDuration);
    }
    if (event.target.closest('.green__pipe')) {
       mario.classList.remove('jump');
       mario.classList.add('duble_jump');
+
+      // Переход на другую страницу после завершения двойного прыжка
+      setTimeout(() => {
+         window.location.href = 'https://azyk.github.io/Game/';
+      }, jumpDuration);
    }
-})
+});
